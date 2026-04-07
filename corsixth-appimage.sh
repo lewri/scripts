d2098d0bb79d802fd2b381564fa366b4cc097e6b
lewri <lewri@users.noreply.github.com>
2026-04-07 19:29:31 +0100

    Fix cmake

--- corsixth-appimage.sh
+++ corsixth-appimage.sh
@@ -50,8 +50,8 @@ cmake \
     -DCMAKE_BUILD_TYPE=Release \
     -DSEARCH_LOCAL_DATADIRS=ON \
     -DUSE_SOURCE_DATADIRS=OFF \
-    -DLUA_INCLUDE_DIR=/usr/include/lua5.4 \
-    -DLUA_LIBRARY=/usr/lib/x86_64-linux-gnu/liblua5.4.so \
+    -DLUA_INCLUDE_DIR='/usr/include/lua5.4' \
+    -DLUA_LIBRARY='/usr/lib/x86_64-linux-gnu/liblua5.4.so' \
     -DCMAKE_INSTALL_RPATH='\$ORIGIN/../lib' \
     ..
 make -j"$(nproc)"
